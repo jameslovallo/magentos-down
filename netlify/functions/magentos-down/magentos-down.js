ebf7728926bcddde36f0598e8605cc4a6b262238
James Lovallo <patrickonparker@gmail.com>
2022-04-07 09:23:41 -0400

    initial commit

---
+++ netlify/functions/magentos-down/magentos-down.js
@@ -0,0 +1,53 @@
+const rootRespone = require('./json/root-response.json')
+const singleProduct = require('./json/single-product.json')
+const productAggregations = require('./json/product-aggregations.json')
+const productListingItems = require('./json/product-listing-items.json')
+const customAttributeMetadata = require('./json/custom-attribute-metadata.json')
+
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const handler = async (event) => {
+	try {
+		const params = event.queryStringParameters
+
+		let body
+
+		if (!params.query) body = JSON.stringify(rootRespone)
+
+		if (params.query.startsWith('{ products( filter: { sku:')) {
+			body = JSON.stringify(singleProduct)
+		}
+
+		if (
+			params.query.startsWith('{ products(filter: { category_id:') &&
+			params.query.includes('aggregations')
+		) {
+			body = JSON.stringify(productAggregations)
+		}
+
+		if (
+			params.query.startsWith('{ products( filter: { category_id:') &&
+			params.query.includes('{ items { uid sku name')
+		) {
+			body = JSON.stringify(productListingItems)
+		}
+
+		if (params.query.startsWith('{ customAttributeMetadata')) {
+			body = JSON.stringify(customAttributeMetadata)
+		}
+
+		return {
+			statusCode: 200,
+			headers: {
+				'Access-Control-Allow-Origin': '*', // Allow from anywhere
+				'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
+				'Access-Control-Allow-Headers':
+					'Content-Type, Authorization, X-Requested-With',
+			},
+			body: body,
+		}
+	} catch (error) {
+		return { statusCode: 500, body: error.toString() }
+	}
+}
+
+module.exports = { handler }
